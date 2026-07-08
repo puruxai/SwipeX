4d9c0b306d394dfdd855dc664677c7b11a2f33ca
Purusottam Dash <purusottamdash0@gmail.com>
2026-07-08 17:47:00 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'https://swipex.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Interceptor to add JWT Auth token
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('swipex_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('swipex_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
